305d9c5a88bdde6726060692a659e507ad5dc0bb
Chris Maher <chrismaher.wit@gmail.com>
2016-02-24 18:41:27 +0000

    Start Brewing...

---
+++ app/src/main/java/com/chris/app/saveme/MainActivity.java
@@ -0,0 +1,512 @@
+package com.chris.app.saveme;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.chris.app.saveme.adapters.VolleyAdapter;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private SharedPreferences settings;
+    private String userName, userEmail;
+    // Google +
+    private static final int RC_SIGN_IN = 0;
+    private static final String TAG1 = "MainActivity";
+    // Profile pic image size in pixels
+    private static final int PROFILE_PIC_SIZE = 400;
+    // Google client to interact with Google API
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mIntentInProgress;
+    private boolean mSignInClicked;
+    private ConnectionResult mConnectionResult;
+    private MenuItem facebookMenuButton, googleMenuButton;
+
+    // Creating Facebook CallbackManager Value
+    public static CallbackManager callbackmanager;
+
+    ArrayList<HashMap<String, String>> oslist = new ArrayList<HashMap<String, String>>();
+    ListView list;
+
+    // json array response url
+    private String urlJsonArry = "http://coffeemate.club/api/coffees";
+
+    private static String TAG = MainActivity.class.getSimpleName();
+    private Button btnMakeObjectRequest, btnMakeArrayRequest;
+
+    // Progress dialog
+    private ProgressDialog pDialog;
+
+    private TextView txtResponse;
+
+    // temporary string to show the parsed response
+    private String jsonResponse;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Initialize SDK before setContentView(Layout ID)
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        // Get SharedPreference Login
+        settings = getSharedPreferences("login_details", 0);
+        userEmail = settings.getString("email", "");
+        userName = settings.getString("name", "");
+
+        // Initializing google plus api client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this).addApi(Plus.API)
+                .addScope(Plus.SCOPE_PLUS_LOGIN).build();
+
+        oslist = new ArrayList<HashMap<String, String>>();
+
+        pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Please wait...");
+        pDialog.setCancelable(false);
+
+        makeJsonArrayRequest();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        facebookMenuButton = menu.findItem(R.id.action_facebook_login);
+        googleMenuButton = menu.findItem(R.id.action_google_login);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_facebook_login) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
+
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
+        }
+
+        //        callbackmanager.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+    public void loginWithGoogle(MenuItem item) {
+
+        signInWithGplus();
+
+    }
+
+    /**
+     * Sign-in into google
+     */
+    private void signInWithGplus() {
+        if (!mGoogleApiClient.isConnecting()) {
+            mSignInClicked = true;
+            resolveSignInError();
+        }
+    }
+
+    /**
+     * Method to resolve any signin errors
+     */
+    private void resolveSignInError() {
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+            } catch (IntentSender.SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
+        }
+    }
+
+    /**
+     * Fetching user's information name, email, profile pic
+     */
+    private void getProfileInformation() {
+        try {
+            if (Plus.PeopleApi.getCurrentPerson(mGoogleApiClient) != null) {
+                Person currentPerson = Plus.PeopleApi
+                        .getCurrentPerson(mGoogleApiClient);
+                String personName = currentPerson.getDisplayName();
+                String personPhotoUrl = currentPerson.getImage().getUrl();
+                String personGooglePlusProfile = currentPerson.getUrl();
+                String email = Plus.AccountApi.getAccountName(mGoogleApiClient);
+
+                Log.e(TAG1, "Name: " + personName + ", plusProfile: "
+                        + personGooglePlusProfile + ", email: " + email
+                        + ", Image: " + personPhotoUrl);
+
+                userName = personName;
+                userEmail = email;
+
+                // by default the profile url gives 50x50 px image only
+                // we can replace the value with whatever dimension we want by
+                // replacing sz=X
+                personPhotoUrl = personPhotoUrl.substring(0,
+                        personPhotoUrl.length() - 2)
+                        + PROFILE_PIC_SIZE;
+
+                //   new LoadProfileImage(imgProfilePic).execute(personPhotoUrl);
+
+            } else {
+//                Toast.makeText(getApplicationContext(),
+//                        "Person information is null", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Background Async task to load user profile picture from url
+     */
+    private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public LoadProfileImage(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle arg0) {
+        mSignInClicked = false;
+
+        // Get user's information
+        getProfileInformation();
+
+        // Update the UI after signin
+        updateGui(true);
+
+        // Welcome
+    //    Toast.makeText(this, "Welcome, " + userName, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int arg0) {
+        mGoogleApiClient.connect();
+        updateGui(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    public void loginWithFacebook(MenuItem item) {
+
+        onFblogin();
+
+    }
+
+    private void onFblogin() {
+        callbackmanager = CallbackManager.Factory.create();
+
+        // Set permissions
+        LoginManager.getInstance().logInWithReadPermissions(this, Arrays.asList("email", "public_profile"));
+
+        LoginManager.getInstance().registerCallback(callbackmanager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+
+
+                        GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+                                try {
+                                    String email = jsonObject.getString("email");
+                                    String fullName = jsonObject.getString("name");
+
+                                    userEmail = email;
+                                    userName = fullName;
+
+                                    // Save your info
+                                    settings = getSharedPreferences("login_details", 0);
+                                    SharedPreferences.Editor editor = settings.edit();
+                                    editor.putString("email", email);
+                                    editor.putString("name", fullName);
+                                    editor.commit();
+
+
+                                } catch (Exception e) {
+                                    Log.d("FacebookActivity", "onCompleted - undetermined FB exception");
+                                }
+
+                            }
+                        });
+
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,email");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+
+                        updateGui(true);
+                    //    Toast.makeText(MainActivity.this, "Welcome, " + userName, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.d("TAG_CANCEL", "On cancel");
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Log.d("TAG_ERROR", error.toString());
+                    }
+                });
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (!result.hasResolution()) {
+            GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
+                    0).show();
+            return;
+        }
+
+        if (!mIntentInProgress) {
+            // Store the ConnectionResult for later usage
+            mConnectionResult = result;
+
+            if (mSignInClicked) {
+                // The user has already clicked 'sign-in' so we attempt to
+                // resolve all
+                // errors until the user is signed in, or they cancel.
+                resolveSignInError();
+            }
+        }
+
+    }
+
+    private void updateGui(boolean update) {
+
+        facebookMenuButton.setVisible(false);
+        googleMenuButton.setVisible(false);
+
+    }
+
+    private void makeJsonArrayRequest() {
+
+        showpDialog();
+
+        JsonArrayRequest req = new JsonArrayRequest(urlJsonArry,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        try {
+                            // Parsing json array response
+                            // loop through each json object
+                            jsonResponse = "";
+                            for (int i = 0; i < response.length(); i++) {
+
+                                JSONObject dealObject = (JSONObject) response.get(i);
+
+                                String dealTitle = dealObject.getString("title");
+                                String dealDetails = dealObject.getString("details");
+                                JSONObject dealUserObject = dealObject.getJSONObject("user");
+                                String dealUsername = dealUserObject.getString("displayName");
+                                int dealVotes = dealObject.getInt("votes");
+                                double dealPrice = dealObject.getDouble("price");
+                                String dealRetailer = dealObject.getString("retailer");
+
+                                ImageView thumbnail = (ImageView) findViewById(R.id.thumbnail);
+
+                                jsonResponse += "Name: " + dealTitle + "\n\n";
+                                jsonResponse += "Email: " + dealDetails + "\n\n";
+                                jsonResponse += "Home: " + dealUsername + "\n\n";
+                                jsonResponse += "Votes: " + dealVotes + "\n\n\n";
+                                jsonResponse += "Price: " + dealVotes + "\n\n\n\n";
+                                jsonResponse += "Retailer: " + dealRetailer + "\n\n\n\n\n";
+
+                                HashMap<String, String> map = new HashMap<String, String>();
+
+                                map.put("1", dealTitle);
+                                map.put("2", dealUsername);
+                                map.put("3", dealVotes+"");
+                                map.put("4", dealDetails);
+                                map.put("5", "£"+dealPrice);
+                                map.put("6", "@ " + dealRetailer);
+
+                                oslist.add(map);
+                                list=(ListView)findViewById(R.id.listView);
+
+                                ListAdapter adapter = new SimpleAdapter(MainActivity.this, oslist,
+                                        R.layout.list_item_1,
+                                        new String[] { "1", "2", "3", "5", "6"}, new int[] {
+                                        R.id.title,  R.id.name, R.id.votes, R.id.price, R.id.retailer });
+
+
+                                list.setAdapter(adapter);
+
+
+                                list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                                    @Override
+                                    public void onItemClick(AdapterView<?> parent, View view,
+                                                            int position, long id) {
+                                        Toast.makeText(MainActivity.this, "You Clicked at "+oslist.get(+position).get("1"), Toast.LENGTH_SHORT).show();
+
+                                    }
+                                });
+
+                            }
+
+//                            txtResponse.setText(jsonResponse);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),
+                                    "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        hidepDialog();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(),
+                        error.getMessage(), Toast.LENGTH_SHORT).show();
+                hidepDialog();
+            }
+        });
+
+        // Adding request to request queue
+        VolleyAdapter.getInstance().addToRequestQueue(req);
+    }
+
+    private void showpDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hidepDialog() {
+        if (pDialog.isShowing())
+            pDialog.dismiss();
+    }
+
+}
